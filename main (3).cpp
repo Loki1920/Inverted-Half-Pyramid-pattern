9b8e83bb498fbc51aa6c03fc00803d45b4a1b7ce
LUCKY TIWARI <89370329+Loki1920@users.noreply.github.com>
2022-03-25 20:25:51 +0530

    Add files via upload

---
+++ main (3).cpp
@@ -0,0 +1,19 @@
+// print Inverted Half pyramid using star
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int row,column;
+  cout<<"enter row:";
+  cin>>row;
+
+  for(int i=row;i>=1;i--){
+      for(int j=1;j<=i;j++){
+        cout<<"* ";
+      }
+  cout<<endl;
+    }
+  return 0;
+  
+}
